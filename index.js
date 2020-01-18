c816640e6e9747eaae46a9b8385a554b7cd11340
Boi Moriba <boi.moriba@gmail.com>
2020-01-18 04:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ function sayHiToGrandma (string) {
 if (string.toLowerCase){
   return "I can't hear you!"
   }
-  else if (string.toUpperCase){
-    return "YES INDEED!"
-  }
-  else if (sring = "I love you Grandma.") {
-  return "I love you, too."
-  }
+else if (string.toUpperCase()){
+  return "YES INDEED!"
 }
