ffbddd0b6e95774043ced4a1c707c25cecdae234
Boi Moriba <boi.moriba@gmail.com>
2020-01-18 04:34:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function logWhisper (string) {
 }
 function sayHiToGrandma (string) {
 if (string.toLowerCase){
-  return "I can't hear you!"
-  }
-  if (string.toUpperCase()){
-    return "YES INDEED!"
+  return "I can\'t hear you!"
   }
 }
